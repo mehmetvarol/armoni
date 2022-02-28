2f82b6b5eb38846701fb0edcf21198d69da5127b
Mehmet VAROL <mehmet.varol@gmail.com>
2022-03-01 00:12:50 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      padding: '0.5rem',
+      center: true,
+    },
+  },
+  plugins: [],
+}
